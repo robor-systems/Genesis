b9b517f9c85c7ba552fb7e54a6a02336d9e9c6d1
Zakria Janjua <zakria.janjua@hotmail.com>
2022-03-12 00:47:34 +0500

    added poster and made the site responsive

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
 import Head from 'next/head';
 
 export default function Home() {
-  const landingVideo = 'https://genesis-landing-page.s3.eu-west-1.amazonaws.com/Token+v1+-+City_Token+V4+web+use+2.mp4';
+  const landingVideo =
+    'https://genesis-landing-page.s3.eu-west-1.amazonaws.com/Token+v1+-+City_Token+V4+web+use+2.mp4';
+  const landingPoster =
+    'https://genesis-landing-page.s3.eu-west-1.amazonaws.com/Token-v1-without-logo.png';
   return (
-    <div className='translate-y-1/2'>
-      <div className='flex justify-center items-center'>
+    <div className='md:translate-y-1/2 translate-y-32'>
+      <div className='md:flex justify-center items-center'>
         <Head>
           <title>Genesis</title>
           <meta name='description' content='Genesis Landing Page' />
         </Head>
-        <video autoPlay loop muted width={230} height={280}>
-          <source src={landingVideo} type='video/mp4' />
-        </video>
-        <div className='ml-12'>
+        <div className='flex justify-center items-center'>
+          <video
+            autoPlay
+            loop
+            muted
+            width={230}
+            height={280}
+            poster={landingPoster}
+          >
+            <source src={landingVideo} type='video/mp4' />
+          </video>
+        </div>
+        <div className='md:ml-12 mt-12'>
           <span className='flex'>
             <h2 className='text-center text-5xl text-regal-blue'>REAL</h2>
             <h1 className='text-center text-5xl text-white ml-3'>WEALTH</h1>
@@ -23,7 +35,7 @@ export default function Home() {
               VIRTUAL
             </h1>
           </span>
-          <span className='flex flex-start mt-5'>
+          <span className='flex justify-center md:justify-start mt-5'>
             <h1 className='text-center text-5xl text-white'>ASSETS</h1>
           </span>
         </div>
